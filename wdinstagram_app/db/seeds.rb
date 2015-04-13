179f318193c45370c690120df3b95f96b5591bee
Lindsey <lmdragun@gmail.com>
2015-04-13 18:24:35 -0400

    basics finished, look not

--- wdinstagram_app/db/seeds.rb
+++ wdinstagram_app/db/seeds.rb
@@ -9,3 +9,4 @@
 Entry.create(author: "Person", photo_url: "http://41.media.tumblr.com/69b5a9060522e711cc16dcfd7cf55eab/tumblr_nmnmub3FzU1tmfruno1_1280.jpg" )
 Entry.create(author: "That Guy", photo_url: "http://36.media.tumblr.com/38221b2e593acf966387649a609eff4b/tumblr_nmkdpwtwF41tmfruno1_400.jpg" )
 Entry.create(author: "Someone Else", photo_url: "http://41.media.tumblr.com/fad668534468fe2b5cdaba33cdb7c9ff/tumblr_natyrwF1vr1tjsts4o1_400.jpg" )
+Entry.create(author: "Cute thing", photo_url: "http://www.greenfudge.org/wp-content/uploads/2012/01/harp-seal-pup-Canada.jpg")
