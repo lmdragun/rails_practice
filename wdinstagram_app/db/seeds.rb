ba9db7adb7fd54b858f01d028c9867d70be3d3e3
Lindsey <lmdragun@gmail.com>
2015-04-13 18:04:09 -0400

    basics done for hw

---
+++ wdinstagram_app/db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Entry.create(author: "Person", photo_url: "http://41.media.tumblr.com/69b5a9060522e711cc16dcfd7cf55eab/tumblr_nmnmub3FzU1tmfruno1_1280.jpg" )
+Entry.create(author: "That Guy", photo_url: "http://36.media.tumblr.com/38221b2e593acf966387649a609eff4b/tumblr_nmkdpwtwF41tmfruno1_400.jpg" )
+Entry.create(author: "Someone Else", photo_url: "http://41.media.tumblr.com/fad668534468fe2b5cdaba33cdb7c9ff/tumblr_natyrwF1vr1tjsts4o1_400.jpg" )
